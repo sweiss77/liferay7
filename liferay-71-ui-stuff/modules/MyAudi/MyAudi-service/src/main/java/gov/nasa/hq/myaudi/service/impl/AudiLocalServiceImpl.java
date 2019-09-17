68f7f7e0b4579335c29dfd9c0b711521a87fadf1
sweiss <stephen.a.weiss@nasa.gov>
2019-09-17 15:57:14 -0400

    Remove comments

--- liferay-71-ui-stuff/modules/MyAudi/MyAudi-service/src/main/java/gov/nasa/hq/myaudi/service/impl/AudiLocalServiceImpl.java
+++ liferay-71-ui-stuff/modules/MyAudi/MyAudi-service/src/main/java/gov/nasa/hq/myaudi/service/impl/AudiLocalServiceImpl.java
@@ -1,15 +1,3 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser
- * General Public License as published by the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- */
-
 package gov.nasa.hq.myaudi.service.impl;
 
 import java.util.Date;
@@ -33,7 +21,7 @@ import gov.nasa.hq.myaudi.service.base.AudiLocalServiceBaseImpl;
  * credentials because this service can only be accessed from within the same VM.
  * </p>
  *
- * @author Brian Wing Shun Chan
+ * @author Steve weiss
  * @see AudiLocalServiceBaseImpl
  * @see gov.nasa.hq.myaudi.service.AudiLocalServiceUtil
  */
